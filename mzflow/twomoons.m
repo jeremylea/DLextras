5b9a4ccc57d484c79f8b0b37d27ee637b08613f0
Jeremy Lea <jeremylea@gmail.com>
2023-05-21 14:49:56 -0700

    Make the two moons function more like the python one

--- mzflow/twomoons.m
+++ mzflow/twomoons.m
@@ -1,15 +1,15 @@
 function [X,label] = twomoons(n) % Generate two moons, with n points in each moon.
 
 % Specify the radius and relevant angles for the two moons.
-noise = (1/6).*randn(n,1);
-radius = 1 + noise;
-angle1 = pi + pi/10;
-angle2 = pi/10;
+noise = 0.05;
+radius = 1;
+angle1 = pi;  %+ pi/10;
+angle2 = 0; %pi/10;
 
 % Create the bottom moon with a center at (1,0).
 bottomTheta = linspace(-angle1,angle2,n)';
-bottomX1 = radius.*cos(bottomTheta) + 1;
-bottomX2 = radius.*sin(bottomTheta);
+bottomX1 = radius.*cos(bottomTheta) + 1.0;
+bottomX2 = radius.*sin(bottomTheta) + 0.5;
 
 % Create the top moon with a center at (0,0).
 topTheta = linspace(angle1,-angle2,n)';
@@ -19,4 +19,11 @@ topX2 = radius.*sin(topTheta);
 % Return the moon points and their labels.
 X = [bottomX1 bottomX2; topX1 topX2];
 label = [ones(n,1); 2*ones(n,1)];
+
+idx = randperm(numel(label));
+X = X(idx, :);
+label = label(idx);
+
+X = X + noise * randn(size(X));
+
 end
