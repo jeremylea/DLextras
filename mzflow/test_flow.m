1e63f98f45b4c13bd1458bfa51228c2cda6fcf67
Jeremy Lea <jeremylea@gmail.com>
2023-05-21 15:16:24 -0700

    Improve the tests

--- mzflow/test_flow.m
+++ mzflow/test_flow.m
@@ -1,14 +1,50 @@
 rng("default") % For reproducibility
-[X,label] = twomoons(200);
+
+close all;
+
+X = betarnd(2,2,10000,1);
+data = array2table(X,"VariableNames",{"x"});
+
+fig = figure;
+hold on;
+histogram(X,20,"Normalization","pdf");
+flow = Flow("x");
+flow = flow.set_default_bijector(data);
+flow.sample(1);
+[flow,losses] = flow.train(data,[],10);
+samples = flow.sample(10000);
+histogram(samples{:,1},20,"Normalization","pdf");
+%grid = jnp.linspace(-2, 2, 100);
+%pdfs = flow.posterior(data, column="x", grid=grid);
+hold off;
+pause
+close(fig);
+
+[X,label] = twomoons(10000);
 data = array2table([X label],"VariableNames",{"x","y","label"});
 test = array2table(unique(label),"VariableNames",{"label"});
 
-%scatter(X(:,1),X(:,2),[],label,"filled")
+scatter(X(:,1),X(:,2),[],"red","filled")
 %title("Labeled Data")
+drawnow;
+pause(1);
+flow = Flow(["x", "y"]);
+flow = flow.set_default_bijector(data);
+flow.sample(1);
+[flow,losses] = flow.train(data);
+samples = flow.sample(10000,test);
+scatter(samples{:,1},samples{:,2},[],"red","filled");
+%grid = jnp.linspace(-2, 2, 100);
+%pdfs = flow.posterior(data, column="x", grid=grid);
+pause
+
+scatter(X(:,1),X(:,2),[],label,"filled")
 flow = Flow(["x", "y"],"label");
 flow = flow.set_default_bijector(data);
 flow.sample(1,test);
 [flow,losses] = flow.train(data);
-samples = flow.sample(10,test);
+samples = flow.sample(10000,test);
+scatter(samples{:,1},samples{:,2},[],samples{:,3},"filled");
 %grid = jnp.linspace(-2, 2, 100);
 %pdfs = flow.posterior(data, column="x", grid=grid);
+pause
